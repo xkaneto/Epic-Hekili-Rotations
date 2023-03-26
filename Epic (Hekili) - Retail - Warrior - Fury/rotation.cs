b6df51886bf613078c79362d032d1651ffc7f90d
xkaneto <cechreis@gmail.com>
2023-03-26 20:39:38 +0200

    Message fix

--- Epic (Hekili) - Retail - Warrior - Fury/rotation.cs
+++ Epic (Hekili) - Retail - Warrior - Fury/rotation.cs
@@ -1343,19 +1343,19 @@ namespace AimsharpWow.Modules
             Aimsharp.QuickDelay = 50;
             Aimsharp.SlowDelay = 150;
 
-            Aimsharp.PrintMessage("Epic PVE - Warrior Arms", Color.Yellow);
+            Aimsharp.PrintMessage("Epic PVE - Warrior Fury", Color.Yellow);
             Aimsharp.PrintMessage("This rotation requires the Hekili Addon !", Color.White);
             Aimsharp.PrintMessage("Hekili > Toggles > Unbind everything !", Color.White);
             Aimsharp.PrintMessage("-----", Color.Black);
             Aimsharp.PrintMessage("- Talents -", Color.White);
-            Aimsharp.PrintMessage("Wowhead: https://www.wowhead.com/guide/classes/warrior/arms/overview-pve-dps", Color.Yellow);
+            Aimsharp.PrintMessage("Wowhead: https://www.wowhead.com/guide/classes/warrior/Fury/overview-pve-dps", Color.Yellow);
             Aimsharp.PrintMessage("-----", Color.Black);
             Aimsharp.PrintMessage("- General -", Color.Yellow);
             Aimsharp.PrintMessage("/" + FiveLetters + " NoInterrupts - Disables Interrupts", Color.Yellow);
             Aimsharp.PrintMessage("/" + FiveLetters + " NoCycle - Disables Target Cycle", Color.Yellow);
             Aimsharp.PrintMessage("/" + FiveLetters + " StormBolt - Casts Storm Bolt @ Target next GCD", Color.Yellow);
             Aimsharp.PrintMessage("/" + FiveLetters + " HeroicLeap - Casts Heroic Leap @ Cursor next GCD", Color.Yellow);
-            Aimsharp.PrintMessage("/" + FiveLetters + " QueueRavager - Casts Bladestorm @ next GCD", Color.Yellow);
+            Aimsharp.PrintMessage("/" + FiveLetters + " QueueRavager - Casts Ravager @ next GCD", Color.Yellow);
             Aimsharp.PrintMessage("/" + FiveLetters + " SpearOfBastion - Casts Spear of Bastion @ next GCD", Color.Yellow);
             Aimsharp.PrintMessage("-----", Color.Black);
 
